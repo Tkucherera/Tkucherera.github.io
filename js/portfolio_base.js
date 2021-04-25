ababfc9818964d28569d728cf8e4897f7802f4b1
Tinashe K <tinashedeveloper@gmail.com>
2021-04-25 15:59:56 -0400

    resume

---
+++ js/portfolio_base.js
@@ -0,0 +1,13 @@
+
+//function for navigation menu
+function topnav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+//function for vertical tabs
